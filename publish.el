73d3fa6bf57f8cda6b5b5c8c844bc9473407d49d
Emmanuele Somma <emmanuele@exedre.org>
2025-05-30 20:03:46 +0200

    init

---
+++ publish.el
@@ -0,0 +1,17 @@
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      '(("filosofia-medicina"
+         :base-directory "~/Dropbox/LIFE/10-projects/10.50 infomedia site/it.infomedia.lm/org"
+         :publishing-directory "~/Dropbox/LIFE/10-projects/10.50 infomedia site/it.infomedia.lm/html"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :with-author t
+         :with-creator t
+         :section-numbers nil
+         :with-toc nil
+         :html-head "<link rel=\"stylesheet\" href=\"../css/tufte.css\" />"
+         :html-html5-fancy t
+         :html-doctype "html5")))
+
+(org-publish-all t)
