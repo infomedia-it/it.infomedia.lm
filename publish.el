6ed6c830fdadb5ac039ff12a40fd719b1d689bf5
Emmanuele Somma <emmanuele@exedre.org>
2025-05-31 18:24:55 +0200

    step

--- publish.el
+++ publish.el
@@ -6,7 +6,7 @@
          :publishing-directory "~/Dropbox/LIFE/10-projects/10.50 infomedia site/it.infomedia.lm/html"
          :recursive t
          :publishing-function org-html-publish-to-html
-         :with-author t
+         :with-author nil
          :with-creator nil
          :with-date nil
          :section-numbers nil
